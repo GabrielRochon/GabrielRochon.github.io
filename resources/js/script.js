d8c546ce3c985e3d3352b3101b7a50d0771ac116
Gabriel <rochon.gabriel1@gmail.com>
2020-07-05 16:50:21 -0400

    Added scroll to section jQuery

--- resources/js/script.js
+++ resources/js/script.js
@@ -1,5 +1,20 @@
 $(document).ready(function() {
     
+    /* Hero buttons scroll to sections */
+    $('.js--scroll-to-projects').click(function () {
+       $('html, body').animate({scrollTop: $('.js--section-projects').offset().top}, 1000); 
+    });
     
+    $('.js--scroll-to-skills').click(function () {
+       $('html, body').animate({scrollTop: $('.js--section-skills').offset().top}, 1000); 
+    });
+    
+    $('.js--scroll-to-resume').click(function () {
+       $('html, body').animate({scrollTop: $('.js--section-resume').offset().top}, 1000); 
+    });
+    
+    $('.js--scroll-to-contact').click(function () {
+       $('html, body').animate({scrollTop: $('.js--section-contact').offset().top}, 1000); 
+    });
     
 });
